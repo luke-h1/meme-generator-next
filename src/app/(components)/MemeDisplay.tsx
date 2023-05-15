b5e9cdd3e495daedad697a2726cc4ee4eb72ecc7
luke-h1 <luke.howsam@yahoo.com>
2023-05-15 22:17:02 +0100

    feat(frontend): responsiveness

--- src/app/(components)/MemeDisplay.tsx
+++ src/app/(components)/MemeDisplay.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import { Anton } from 'next/font/google';
 import Image from 'next/image';
+import { useElementSize } from 'usehooks-ts';
 
 const anton = Anton({ subsets: ['latin'], weight: '400' });
 
@@ -23,8 +26,12 @@ interface Props {
 }
 
 const MemeDisplay = ({ background, textBlocks }: Props) => {
+  const [memeRef, { width }] = useElementSize();
+
+  const ratio = width / background.width;
+
   return (
-    <div className="relative">
+    <div className="relative" ref={memeRef}>
       <Image
         src={background.src}
         width={background.width}
@@ -37,16 +44,16 @@ const MemeDisplay = ({ background, textBlocks }: Props) => {
             key={textBlock.id}
             className="absolute"
             style={{
-              top: textBlock.top,
-              left: textBlock.left,
-              width: textBlock.width,
-              height: textBlock.height,
+              top: textBlock.top * ratio,
+              left: textBlock.left * ratio,
+              width: textBlock.width * ratio,
+              height: textBlock.height * ratio,
             }}
           >
             <div
               className={`${anton.className} text-center text-white text-stroke-white`}
               style={{
-                fontSize: textBlock.fontSize,
+                fontSize: textBlock.fontSize * ratio,
                 lineHeight: '1.1',
               }}
             >
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-import MemeDisplay from '@frontend/components/MemeDisplay';
+import MemeDisplay from '@frontend/app/(components)/MemeDisplay';
 
 const Home = () => {
   return (
-    <main className="max-w-[1200px] mx-auto">
+    <main
+      className="max-w-[1200px] mx-auto">
       <MemeDisplay
         background={{
           src: '/disaster-girl.jpg',
@@ -10,6 +11,18 @@ const Home = () => {
           height: 900,
           alt: 'Disaster Girl',
         }}
+        textBlocks={[
+          {
+            id: 'saying',
+            top: 750,
+            left: 100,
+            width: 1000,
+            height: 100,
+            fontSize: 100,
+            color: 'white',
+            text: 'Im gonna be rich',
+          },
+        ]}
       />
     </main>
   );
